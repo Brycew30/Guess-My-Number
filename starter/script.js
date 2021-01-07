3c80fb441860c1eac82814de2f5f534f85d39f16
Brycew30 <brycew30@gmail.com>
2021-01-07 11:05:42 -0600

    implement high score functionality

--- starter/script.js
+++ starter/script.js
@@ -8,6 +8,7 @@
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,6 +24,10 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
 
     // When the guess is too high
   } else if (guess > secretNumber) {
